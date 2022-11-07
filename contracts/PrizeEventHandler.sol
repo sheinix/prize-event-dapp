436ede253742f69b21840079c1073fc92a042033
damienteo <damien.teo.ks@gmail.com>
2022-11-07 22:50:39 +0800

    Remove reference to prizeToken

--- contracts/PrizeEventHandler.sol
+++ contracts/PrizeEventHandler.sol
@@ -6,20 +6,9 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-// NOTE: We could be using directly VotingToken but there;s a weird solidity warning bug (https://github.com/ethereum/solidity/issues/11522)
 import "./VotingToken.sol";
 
 // TODO: Use referenceBlock parameter in voting for security
-
-interface ITokenizedVotes {
-    function getPastVotes(address, uint256) external view returns (uint256);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-}
 
 error PrizeEventHandler__TooManyParticipantsAtOnce();
 error PrizeEventHandler__ApprovalFailed(address token, uint256 amount);
@@ -46,7 +35,6 @@ contract PrizeEventHandler is AccessControl {
         address owner;
         uint256 prizeAmount;
         uint256 referenceBlock;
-        IERC20 prizeToken;
         // @notice Array of numbers that reprsesents the distribution of price ([50%, 30%, 20%] or [100%] or [40%, 30%, 20%, 10%] etc.)
         uint256[] winnersDistribution;
         address[] voters;
@@ -76,6 +64,8 @@ contract PrizeEventHandler is AccessControl {
     // @notice the nested mapping:
     // participant Address -> map of TokenPrize to Balance in that token - used to claim prizes for winners.
     mapping(address => mapping(IERC20 => uint256)) public s_participantBalances;
+
+    uint256 tokenPriceInWei = 0.1 ether;
 
     VotingToken public s_votingToken;
 
@@ -146,38 +136,17 @@ contract PrizeEventHandler is AccessControl {
     function setupEvent(
         uint256 _prizeAmount,
         uint256 _referenceBlock,
-        address _prizeToken,
         uint256[] memory _winnersDistribution,
         address[] memory _voters,
         address[] memory _participants
     ) public validParticipants(_participants) {
-        // (Needs approval first)
-        // Transfer the Token:
-        IERC20 tokenPrize = IERC20(_prizeToken);
-
-        if (!tokenPrize.transferFrom(msg.sender, address(this), _prizeAmount)) {
-            revert PrizeEventHandler__RegistrationFailed(_prizeToken, _prizeAmount);
-        }
-
         // If succeed create the storage:
-        registerEvent(
-            _prizeAmount,
-            _referenceBlock,
-            _prizeToken,
-            _winnersDistribution,
-            _voters,
-            _participants
-        );
-
-        //s_votingToken.grantRole(MINTER_ROLE, msg.sender);
-
-        // TODO: Assign Voting Token Minter Role to the event creator.
+        registerEvent(_prizeAmount, _referenceBlock, _winnersDistribution, _voters, _participants);
     }
 
     function registerEvent(
         uint256 _prizeAmount,
         uint256 _referenceBlock,
-        address _prizeToken,
         uint256[] memory _winnersDistribution,
         address[] memory _voters,
         address[] memory _participants
@@ -193,7 +162,6 @@ contract PrizeEventHandler is AccessControl {
             msg.sender,
             _prizeAmount,
             _referenceBlock,
-            IERC20(_prizeToken),
             _winnersDistribution,
             _voters,
             _participants,
@@ -261,12 +229,7 @@ contract PrizeEventHandler is AccessControl {
 
         // TODO: what happens when we have ties? --> Out of Scope
 
-        distributePrize(
-            winners,
-            prizeEvent.winnersDistribution,
-            prizeAmount,
-            prizeEvent.prizeToken
-        );
+        distributePrize(winners, prizeEvent.winnersDistribution, prizeAmount);
 
         emit PrizeEventClosed(eventId, prizeAmount);
     }
@@ -274,18 +237,16 @@ contract PrizeEventHandler is AccessControl {
     function distributePrize(
         address[] memory winners,
         uint256[] memory winnersDistribution,
-        uint256 prizeAmount,
-        IERC20 tokenPrize
+        uint256 prizeAmount
     ) private {
         require(
             winnersDistribution.length == winners.length,
             "Winners Array & Distribution Array must have the same length"
         );
 
-        for (uint256 i = 0; i < winners.length; i++) {
-            uint256 prizeDist = prizeAmount.div(100).mul(winnersDistribution[i]);
-            s_participantBalances[winners[i]][tokenPrize] = prizeDist;
-        }
+        // for (uint256 i = 0; i < winners.length; i++) {
+        //     uint256 prizeDist = prizeAmount.div(100).mul(winnersDistribution[i]);
+        // }
     }
 
     /**
@@ -357,10 +318,6 @@ contract PrizeEventHandler is AccessControl {
         returns (uint256)
     {
         return s_participantVotes[participant][eventId];
-    }
-
-    function getPrizeTokenAddressFor(uint256 eventId) public view returns (IERC20) {
-        return s_prizeEvents[eventId].prizeToken;
     }
 
     function addressExists(address addressaToFind, address[] memory addressesArray)
--- test/PrizeEventHandler.test.ts
+++ test/PrizeEventHandler.test.ts
@@ -4,38 +4,35 @@ import { expect, assert, Assertion } from "chai"
 import { ethers, getNamedAccounts } from "hardhat"
 import deployContracts from "../scripts/deploy"
 import { BigNumber, Signer } from "ethers"
-import { PrizeEventHandler, VotingToken, TestToken } from "../typechain-types"
+import { PrizeEventHandler, VotingToken } from "../typechain-types"
 import { hrtime } from "process"
 
 describe("PrizeEventHandler", function () {
     let prizeAmount: BigNumber
     let referenceBlock: BigNumber
-    let daiAddress: string
     let winnersDistribution: bigint[] = []
     var voters: string[]
     var participants: string[]
     let totalSupply: string
     let prizeEventContract: PrizeEventHandler
-    let voteToken: VotingToken
-    let testToken: TestToken
+    let votingToken: VotingToken
     let minterRole: string
 
     beforeEach(async () => {
+        totalSupply = ethers.utils.parseEther("100000").toString() //(10 ** 9).toString()
+
         // Deploy Contracts:
         const VotingToken = await ethers.getContractFactory("VotingToken")
-        voteToken = await VotingToken.deploy()
+        votingToken = await VotingToken.deploy()
+        await votingToken.deployed()
+
         const PrizeEventHandler = await ethers.getContractFactory("PrizeEventHandler")
-        prizeEventContract = await PrizeEventHandler.deploy(voteToken.address)
+        prizeEventContract = await PrizeEventHandler.deploy(votingToken.address)
         await prizeEventContract.deployed()
-        const TestToken = await ethers.getContractFactory("TestToken")
-        totalSupply = ethers.utils.parseEther("100000").toString() //(10 ** 9).toString()
-        testToken = await TestToken.deploy(totalSupply)
-        await testToken.deployed()
 
         // Prepare values:
         prizeAmount = ethers.utils.parseEther("1")
         referenceBlock = BigNumber.from(2000000)
-        daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
         winnersDistribution.push(BigInt(50))
         winnersDistribution.push(BigInt(30))
         winnersDistribution.push(BigInt(20))
@@ -46,19 +43,6 @@ describe("PrizeEventHandler", function () {
 
     describe("Event Registration", function () {
         describe("Register", function () {
-            it("Should revert with insufficent allowance", async function () {
-                await expect(
-                    prizeEventContract.setupEvent(
-                        prizeAmount,
-                        referenceBlock,
-                        testToken.address,
-                        winnersDistribution,
-                        voters,
-                        participants
-                    )
-                ).to.be.revertedWith(`ERC20: insufficient allowance`)
-            })
-
             it("Should revert with too many participants failed", async function () {
                 const { participant1Addr } = await getNamedAccounts()
                 for (let i = 0; i < 11; i++) {
@@ -68,7 +52,6 @@ describe("PrizeEventHandler", function () {
                     prizeEventContract.setupEvent(
                         prizeAmount,
                         referenceBlock,
-                        daiAddress,
                         winnersDistribution,
                         voters,
                         participants
@@ -79,36 +62,13 @@ describe("PrizeEventHandler", function () {
                 )
             })
 
-            it("Should deposit token and Register Event", async function () {
-                await testToken.approve(prizeEventContract.address, totalSupply)
-
-                await prizeEventContract.setupEvent(
-                    prizeAmount,
-                    referenceBlock,
-                    testToken.address,
-                    winnersDistribution,
-                    voters,
-                    participants
-                )
-
-                // Assert
-                // Event created:
-                const prizeEvent = await prizeEventContract.getPrizeEvent(1)
-                assert.equal(prizeEvent[0].toString(), "0")
-
-                // Token Deposited:
-                const balanceOfContract = await testToken.balanceOf(prizeEventContract.address)
-                assert.equal(balanceOfContract.toString(), prizeAmount.toString())
-            })
-
             it("Should emit a prize event created event", async function () {
-                await testToken.approve(prizeEventContract.address, totalSupply)
+                await votingToken.approve(prizeEventContract.address, totalSupply)
 
                 expect(
                     await prizeEventContract.setupEvent(
                         prizeAmount,
                         referenceBlock,
-                        testToken.address,
                         winnersDistribution,
                         voters,
                         participants
@@ -138,11 +98,10 @@ describe("PrizeEventHandler", function () {
             const nonVoter = await ethers.getSigner(nonVoterAddr)
             voters.push(voter1Addr)
 
-            await testToken.approve(prizeEventContract.address, totalSupply)
+            await votingToken.approve(prizeEventContract.address, totalSupply)
             await prizeEventContract.setupEvent(
                 prizeAmount,
                 referenceBlock,
-                testToken.address,
                 winnersDistribution,
                 voters,
                 participants
@@ -162,11 +121,10 @@ describe("PrizeEventHandler", function () {
             const voter = await ethers.getSigner(voter1Addr)
             voters.push(voter1Addr)
 
-            await testToken.approve(prizeEventContract.address, totalSupply)
+            await votingToken.approve(prizeEventContract.address, totalSupply)
             await prizeEventContract.setupEvent(
                 prizeAmount,
                 referenceBlock,
-                testToken.address,
                 winnersDistribution,
                 voters,
                 participants
@@ -183,11 +141,10 @@ describe("PrizeEventHandler", function () {
             const voter1 = await ethers.getSigner(voter1Addr)
             voters.push(voter1Addr)
 
-            await testToken.approve(prizeEventContract.address, totalSupply)
+            await votingToken.approve(prizeEventContract.address, totalSupply)
             await prizeEventContract.setupEvent(
                 prizeAmount,
                 referenceBlock,
-                testToken.address,
                 winnersDistribution,
                 voters,
                 participants
@@ -195,16 +152,16 @@ describe("PrizeEventHandler", function () {
 
             // mint some vote tokens:
             const voteAmount = ethers.utils.parseEther("20")
-            await voteToken.grantRole(minterRole, voter1Addr)
-            await voteToken.connect(voter1).mint(voter1Addr, voteAmount)
-            await voteToken.connect(voter1).approve(prizeEventContract.address, voteAmount)
+            await votingToken.grantRole(minterRole, voter1Addr)
+            await votingToken.connect(voter1).mint(voter1Addr, voteAmount)
+            await votingToken.connect(voter1).approve(prizeEventContract.address, voteAmount)
             // Connect with registered voter and vote for participant1
             await prizeEventContract.connect(voter1).vote(0, participant1Addr, voteAmount)
 
             // Validate vote token balance has been transfered to contract & participant1 votes are registered
-            assert.equal((await voteToken.balanceOf(voter1Addr)).toString(), "0")
+            assert.equal((await votingToken.balanceOf(voter1Addr)).toString(), "0")
             assert.equal(
-                (await voteToken.balanceOf(prizeEventContract.address)).toString(),
+                (await votingToken.balanceOf(prizeEventContract.address)).toString(),
                 voteAmount.toString()
             )
             assert.equal(
@@ -221,11 +178,10 @@ describe("PrizeEventHandler", function () {
             const voter = await ethers.getSigner(voter1Addr)
             voters.push(voter1Addr)
 
-            await testToken.approve(prizeEventContract.address, totalSupply)
+            await votingToken.approve(prizeEventContract.address, totalSupply)
             await prizeEventContract.setupEvent(
                 prizeAmount,
                 referenceBlock,
-                testToken.address,
                 winnersDistribution,
                 voters,
                 participants
@@ -243,11 +199,11 @@ describe("PrizeEventHandler", function () {
     //     const nonVoter = await ethers.getSigner(nonVoterAddr)
     //     voters.push(voter1Addr)
 
-    //     await testToken.approve(prizeEventContract.address, totalSupply)
+    //     await votingToken.approve(prizeEventContract.address, totalSupply)
     //     await prizeEventContract.setupEvent(
     //         prizeAmount,
     //         referenceBlock,
-    //         testToken.address,
+    //         votingToken.address,
     //         winnersDistribution,
     //         voters,
     //         participants
